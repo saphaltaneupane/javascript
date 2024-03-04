93c47017f6a1c414501d9aae87a4ceb54e105c9f
saphaltaneupane <neupanesaphalta@gmail.com>
2024-03-04 15:12:59 +0545

    ok

---
+++ basics/expressions.js
@@ -0,0 +1,29 @@
+// 77;
+// "saphalta";
+// false;
+console.log("Operators in js ");
+console.log("Arthmetic Opertors: ");
+let a=45;
+let b=4;
+console.log("a+b",a+b);
+console.log("a-b",a-b);
+console.log("a*b",a*b);
+console.log("a/b",a/b); //accurate value like 11.25
+console.log("a**b",a**b);//power
+console.log("++a", ++a);//output:46
+console.log("a++", a++);//output:46
+console.log("--a", --a);//output:46
+console.log("a--", a--);//output:46
+console.log("a", a);//output:45
+console.log("a--", a--);//output:45
+console.log("Assignment Operators: ");
+let ass=3;
+ass+=2;
+console.log(ass);
+console.log("comparision Operators: ");
+let comp1=6;
+let comp2=7;
+console.log("comp1==comp2",comp1==comp2);
+console.log("comp1!=comp2",comp1!=comp2);
+console.log("comp1===comp2",comp1===comp2);
+console.log("comp1!==comp2",comp1!==comp2);
