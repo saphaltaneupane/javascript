8323e6970e43e2fb410bda8e0f53877569404317
saphaltaneupane <neupanesaphalta@gmail.com>
2024-03-09 09:43:19 +0545

    js

--- basics/loops.js
+++ basics/loops.js
@@ -14,3 +14,21 @@ for(let a in obj){
     console.log(a);//it will print key such as ram sita hari shiva
     console.log("Marks of"+a+"are"+obj[a]);//it will print key with value
 }
+//for of loop
+for(let b of "ram"){// it should be in string or array =iterable data structure
+  console.log(b)
+}
+//do while loop
+let n =prompt("enter a number:")
+n=  Number.parseInt(n)
+let i=10// it will print 10 at least one time 
+do{
+    console.log(i)
+    i++
+}while(i<n)
+//while loop
+i=0;
+while(i<n){
+    console.log(i)
+    i++
+}
