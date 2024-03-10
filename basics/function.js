6368e69dfdc28475fe8a382290f608d4460628b9
saphaltaneupane <neupanesaphalta@gmail.com>
2024-03-10 20:49:35 +0545

    js

---
+++ basics/function.js
@@ -0,0 +1,7 @@
+//why to use functions?
+let a=1;
+let b =2;
+let c =3;
+console.log("Average of a and b is" +(a+b)/2)
+console.log("Average of b and c is" +(b+c)/2)
+console.log("Average of a and c is" +(a+c)/2)
