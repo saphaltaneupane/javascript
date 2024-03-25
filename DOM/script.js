5073be2028988a4da9e5996209350fb48dc64202
saphaltaneupane <neupanesaphalta@gmail.com>
2024-03-25 20:53:53 +0545

    javascript

--- DOM/script.js
+++ DOM/script.js
@@ -1,4 +1,20 @@
 // document.write("hello");
-console.log(window);//window object
+// console.log(window);//window object
 // alert("ello")
-console.dir(document)//special methods properties prints, to print object
+// console.dir(document)//special methods properties prints, to print object
+// console.dir(document.body)
+//for dynamic changes we use dom 
+//Dom manipulation
+let heading=document.getElementById("heading");
+console.log(heading);
+ let headings=document.getElementsByClassName("heading");
+console.log(headings);
+let tags=document.getElementsByTagName("p");
+console.dir(tags);
+//more advamce is query selector here we can  pass id/class/tag
+let element=document.querySelector("p")//first element
+console.log(element);
+let elementsAll=document.querySelectorAll("p")//first element
+console.log(elementsAll);
+let elementsclass=document.querySelectorAll(".heading")//first element
+console.log(elementsclass);
