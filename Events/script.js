f4688628955b374d68f305f0e970de0e221e3d79
saphaltaneupane <neupanesaphalta@gmail.com>
2024-03-28 17:18:56 +0545

    javascript

---
+++ Events/script.js
@@ -0,0 +1,11 @@
+let btn1=document.querySelector("#btn1");
+btn1.onclick=()=>{
+    console.log("btn1 was clicked");
+    let a =25;
+    a++;
+    console.log(a);
+}
+let box=document.querySelector("div");
+box.onmouseover=()=>{
+    console.log("ypu are inside div")
+}
